2cdcd0d9d9487bd9a57af5501f59b8768f4cd224
dogpetkid <joshbudd2011@gmail.com>
2017-05-09 10:44:52 -0700

    Revert "Single Connection"
    This reverts commit 4c7c2a1b9842f98fb265ff5a35200f5b3dbe1545.

--- hybridtest.py
+++ hybridtest.py
@@ -4,14 +4,14 @@ from servertest import *
 HOSTself = socket.gethostbyname(socket.gethostname())
 print("Ip: " + HOSTself)
 PORT = 8089##int(input("Port: "))
-HOSTother = '10.72.20.78'##input("Input the other Ip: ")
+HOSTother = '192.168.112.146'##input("Input the other Ip: ")
 
 def finding():
     print("Finding")
-    if True:##with hostme(HOSTself,PORT) as sock:
+    with hostme(HOSTself,PORT) as sock:
         while True:
             try:
-                get = listen(connection.sock)
+                get = listen(sock.connection)
                 print(get)
                 break
             except Exception as e:
@@ -19,25 +19,25 @@ def finding():
 
 def sending():
     print("Sending")
-    if True:##with clientme(HOSTother,PORT) as sock:
+    with clientme(HOSTother,PORT) as sock:
         while True:
             try:
-                send = speak(connection.sock, input("Send: "))
+                send = speak(sock.clientsocket, input("Send: "))
                 break
             except Exception as e:
                 input(str(e))
 
 if bool(input("Type a letter to go host ")):
-    connection = hostme(HOSTself,PORT)
-    connection.sock = connection.serversocket
+    '''connection = hostme(HOSTself,PORT)
+    connection.sock = connection.serversocket'''
     
     while True:
         finding()
         sending()
 
 else:
-    connection = clientme(HOSTother,PORT)
-    connection.sock = connection.clientsocket
+    '''connection = clientme(HOSTother,PORT)
+    connection.sock = connection.clientsocket'''
     
     while True:
         sending()
